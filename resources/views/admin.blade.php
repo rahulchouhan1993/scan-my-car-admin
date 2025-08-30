e4291c97e9714808c8c7ca796da1ec0d69e3d318
Rahul Chouhan <r.chouhan64@gmail.com>
2025-08-30 17:14:22 +0530

    added link for the toaster

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,6 +11,8 @@
         @vite('resources/js/admin.jsx')
         @inertiaHead
         @routes
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet"/>
+
     </head>
      <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     @inertia
